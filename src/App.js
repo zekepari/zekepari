5ff666fd12548ca4bbbd833beafc7eb2e6b2b028
zekepari <pariezekiel@gmail.com>
2023-12-15 02:45:59 +1000

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,144 @@
 import React from 'react';
-import video1 from './assets/grestin.mp4';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import video1 from './assets/Grestin.mp4';
+import profilePicture from './assets/Profile.webp'
 
-const PortfolioItem = ({ title, description, mediaUrl }) => (
-  <div className="max-w-3xl mx-auto mt-8 p-4 bg-white shadow-md rounded-md">
-    <h2 className="text-2xl font-semibold mb-4">{title}</h2>
-    {mediaUrl && (
-      <div className="max-w-full overflow-hidden rounded-md mb-4">
-        {mediaUrl.endsWith('.mp4') ? (
-          <video controls className="w-full">
-            <source src={mediaUrl} type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-        ) : (
-          <img src={mediaUrl} alt={title} className="w-full" />
-        )}
-      </div>
-    )}
-    <p className="text-gray-600">{description}</p>
+const VIDEO_TYPE = 'video';
+
+const MediaComponent = ({ item, index }) => {
+  const isVideo = item.type === VIDEO_TYPE;
+  const isImage = !isVideo;
+
+  return (
+    <div key={index} className="mb-6">
+      {isVideo ? (
+        <video controls className="w-full rounded-md shadow-md">
+          <source src={item.url} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      ) : isImage ? (
+        <img src={item.url} alt={`Project Media ${index}`} className="w-full rounded-md shadow-md" />
+      ) : null}
+    </div>
+  );
+};
+
+const ProjectCard = ({ title, description, media }) => (
+  <div className="p-6 bg-white mb-8 rounded-lg shadow-md border border-gray-200 text-gray-700">
+    <div className="text-3xl font-bold mb-4 text-center text-gray-900">{title}</div>
+    <div className="mb-4">{description}</div>
+    <div className="flex flex-wrap justify-center">
+      {media.map((item, index) => (
+        <MediaComponent key={index} item={item} index={index} />
+      ))}
+    </div>
   </div>
 );
 
-const App = () => {
+const NationalCard = ({ qualifications }) => (
+  <div className="p-6 bg-white mb-8 rounded-lg shadow-md border border-gray-200 text-gray-700">
+    <div className="text-3xl font-bold mb-4 text-center text-gray-900">National</div>
+    {qualifications.map((qualification, index) => (
+      <div key={index} className="mb-2">
+        <div className="text-lg font-semibold">{qualification.qualification}</div>
+        <div className="text-sm text-gray-600">{qualification.graduationDate}</div>
+      </div>
+    ))}
+    <a href={`https://www.aqf.edu.au/framework/aqf-qualifications`} rel="noreferrer noopener" target="_blank" className="hover:text-gray-600">
+      <p className='text-center'>Australian Qualifications Framework <FontAwesomeIcon icon="fa-solid fa-arrow-up-right-from-square" /></p>
+    </a>
+  </div>
+);
+
+const IndustryCard = ({ qualifications }) => (
+  <div className="p-6 bg-white mb-8 rounded-lg shadow-md border border-gray-200 text-gray-700">
+    <div className="text-3xl font-bold mb-4 text-center text-gray-900">Industry</div>
+    {qualifications.map((qualification, index) => (
+      <div key={index} className="mb-2">
+        <div className="text-lg font-semibold">{qualification.qualification}</div>
+        <div className="text-sm text-gray-600">{qualification.graduationDate}</div>
+      </div>
+    ))}
+  </div>
+);
+
+const ContactCard = ({ github, discord }) => {
   return (
-    <div className="min-h-screen bg-gray-100">
-      <header className="bg-blue-500 text-white text-center py-4">
-        <h1 className="text-3xl font-semibold">Zeke Pari's Developer Portfolio</h1>
-      </header>
-      <main className="py-8">
-        <PortfolioItem
-          title="East Grestin Border"
-          description="In the creation of the East Grestin Border, I endeavored to push the boundaries of UI scripting on the ROBLOX platform, focusing on a meticulous customization of the core graphical user interface. I took pride in refining elements like the player list and toolbar, aiming not just for functionality but also for a visually immersive experience that reflects a dedication to detail and aesthetic excellence. The implementation of secure proximity prompts within NPCs and items underscores my commitment to crafting an interactive and secure gaming environment, enriching the overall user experience. Moreover, the integration of an interactive marketplace service, particularly the inclusion of a request feature for the raider pack, demonstrates a forward-thinking approach to in-game economies and user-driven content. Through this project, I aimed to showcase not only technical proficiency in UI scripting but also a passion for innovative design choices that elevate the gaming experience."
-          mediaUrl={video1}
-        />
-      </main>
+    <div className="p-6 bg-white mb-8 rounded-lg shadow-md border border-gray-200 text-gray-700">
+      <div className="flex flex-row justify-center items-center space-x-4">
+        <a href={`https://github.com/${github}`} rel="noreferrer noopener" target="_blank" className="hover:text-gray-600">
+          <FontAwesomeIcon icon="fa-brands fa-github" /> {github}
+        </a>
+        <p><FontAwesomeIcon icon="fa-brands fa-discord" /> {discord}</p>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const projects = [
+    {
+      title: 'Menu/Experience UI',
+      description:
+        'Focus on use of proximity prompt, marketplace service, core gui customization - securely using industry standard methods.',
+      media: [
+        { type: VIDEO_TYPE, url: video1 },
+      ],
+    },
+    {
+      title: 'Asynchronous Wild Spawn',
+      description: 'Random teleport plugin with various configuration conditions and integrations based on events or commands, asynchronously & fast.',
+      media: [
+        { type: VIDEO_TYPE, url: '' }
+      ]
+    }
+  ];
+  const national = [
+    {
+      qualification: 'Bachelor of Computer Science (Minor in Cloud Computing)',
+      graduationDate: 'Current'
+    },
+    {
+      qualification: 'Certificate III in Information Technology',
+      graduationDate: '2023'
+    },
+    {
+      qualification: 'Certificate III in Business', 
+      graduationDate: '2023'
+    }
+  ];
+  const industry = [
+    {
+      qualification: 'Professional Certificate in Front-End Development (Meta)',
+      graduationDate: '2023'
+    }
+  ];
+  const contactInfo = {
+    github: 'zekepari',
+    discord: '@noreplynotice',
+  };
+
+  return (
+    <div className="container mx-auto p-6 text-center sm:text-left max-w-4xl">
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <div className="text-8xl font-bold text-gray-900">Zeke Pari</div>
+          <p className="text-xl text-gray-700 mt-2">ROBLOX, Minecraft, Discord, Web Developer</p>
+        </div>
+        <img src={profilePicture} alt="Profile" className="w-24 h-24 rounded-full ml-4" />
+      </div>
+
+      <div className="text-4xl font-bold mb-8 text-gray-900">My Work</div>
+      {projects.map((project, index) => (
+        <ProjectCard key={index} {...project} />
+      ))}
+
+      <div className="text-4xl font-bold mb-8 text-gray-900">Qualifications</div>
+      <NationalCard qualifications={national} />
+      <IndustryCard qualifications={industry} />
+
+      <div className="text-4xl font-bold mb-8 text-gray-900">Contact</div>
+      <ContactCard {...contactInfo} />
     </div>
   );
 };
