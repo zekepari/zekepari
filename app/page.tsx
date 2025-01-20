cad4244e21156cea948f0979d2a99927105b67e5
Zeke Pari <pariezekiel@gmail.com>
2025-01-20 16:42:31 +1000

    Add clsx package and enhance CollapsibleVideo component with improved height management and error handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import CollapsibleVideo from "@/components/CollapsibleVideo";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { ArrowLeft, CircleAlert, CornerRightDown, ExternalLink, Flag, Globe, Hospital, Mail, Radio, Wrench } from "lucide-react";
+import {
+  Blocks,
+  CircleAlert,
+  CornerRightDown,
+  ExternalLink,
+  Flag,
+  Globe,
+  Hospital,
+  Mail,
+  Radio,
+  Wrench,
+} from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -11,31 +22,42 @@ export default function Home() {
       <section className="space-y-4">
         <div className="flex justify-around items-center">
           <div className="w-full">
-            <h1 className="text-4xl md:text-5xl font-black">Ezekiel (Zeke) Pari</h1>
+            <h1>Ezekiel (Zeke) Pari</h1>
             <p className="md:text-lg opacity-75">Full Stack Developer</p>
-            <hr/>
+            <hr />
             <div className="flex gap-2 mt-2 flex-wrap">
-              <Link href="https://www.linkedin.com/in/ezekiel-pari-96a458258/" target="_blank" className="hover:text-blue-600 transition">
+              <Link
+                href="https://www.linkedin.com/in/ezekiel-pari-96a458258/"
+                target="_blank"
+                className="hover:text-blue-600 transition"
+              >
                 <FontAwesomeIcon icon={faLinkedin} className="size-8" />
               </Link>
-              <Link href="https://github.com/zekepari" target="_blank" className="hover:text-blue-600 transition">
+              <Link
+                href="https://github.com/zekepari"
+                target="_blank"
+                className="hover:text-blue-600 transition"
+              >
                 <FontAwesomeIcon icon={faGithub} className="size-8" />
               </Link>
-              <Link href="mailto:me@zekepari.dev" className="hover:text-blue-600 transition flex gap-1 items-center">
+              <Link
+                href="mailto:me@zekepari.dev"
+                className="hover:text-blue-600 transition flex gap-1 items-center"
+              >
                 <Mail className="size-4" />
                 <p>me@zekepari.dev</p>
               </Link>
             </div>
           </div>
-          
-          <Image 
+
+          <Image
             src="/Zekes.jpg"
             width={200}
             height={200}
             alt="Picture of me"
             className="aspect-square object-cover border p-2 hidden md:block"
           />
-          <Image 
+          <Image
             src="/Zekes.jpg"
             width={150}
             height={150}
@@ -44,48 +66,109 @@ export default function Home() {
           />
         </div>
 
-        <p>My Tech Stack <CornerRightDown className="size-4 inline-block" /></p>
-        
+        <p>
+          My Tech Stack <CornerRightDown className="size-4 inline-block" />
+        </p>
+
         <div className="flex justify-center">
-            <img
-              src="https://skillicons.dev/icons?i=html,css,js,ts,lua,java,python,react,next,mongo,postgresql,vercel,tailwind,prisma&perline=7"
-              alt="Skill icons"
-              className="w-full sm:hidden"
-            />
-            <img
-              src="https://skillicons.dev/icons?i=html,css,js,ts,lua,java,python,react,next,mongo,postgresql,vercel,tailwind,prisma"
-              alt="Skill icons"
-              className="hidden w-full sm:block"
-            />
-          </div>
-      </section>
-      <hr/>
-      <section className="space-y-4">
-        <h2 className="text-3xl md:text-4xl font-bold">Education</h2>
-        <p><CircleAlert className="size-4 inline-block" /> We&apos;re keeping it simple! • <Link href="https://www.aqf.edu.au/framework/aqf-qualifications" className="text-blue-500 hover:text-blue-400" target="_blank">Australian Qualifications Framework <ExternalLink className="inline-block size-4"/></Link></p>
-        <div>
-          <h3 className="text-2xl md:text-3xl font-semibold">Bachelor of Computer Science</h3>
-          <p className="opacity-75">Minor in Cloud Computing • Graduating 2026</p>
-        </div>
-        <div>
-          <h3 className="text-2xl md:text-3xl font-semibold">Certificate in Front-End Development</h3>
-          <Link href="https://www.coursera.org/account/accomplishments/professional-cert/K329R2NDRAS9" className="text-blue-500 hover:text-blue-400" target="_blank">Digital Certificate <ExternalLink className="inline-block size-4"/></Link>
-        </div>
-        <div>
-          <h3 className="text-2xl md:text-3xl font-semibold">IT Specialist - Python</h3>
-          <Link href="https://www.credly.com/badges/4c90f5d6-40e9-4263-a534-d141cba93c69/linked_in?t=sdxccf" className="text-blue-500 hover:text-blue-400" target="_blank">Digital Certificate <ExternalLink className="inline-block size-4"/></Link>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://skillicons.dev/icons?i=html,css,js,ts,lua,java,python,react,next,mongo,postgresql,vercel,tailwind,prisma&perline=7"
+            alt="Skill icons"
+            height={100}
+            width={1000}
+            className="w-full sm:hidden"
+          />
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src="https://skillicons.dev/icons?i=html,css,js,ts,lua,java,python,react,next,mongo,postgresql,vercel,tailwind,prisma"
+            alt="Skill icons"
+            height={100}
+            width={1000}
+            className="hidden w-full sm:block"
+          />
         </div>
       </section>
-      <hr/>
+      <hr />
       <section className="space-y-4">
-        <h2 className="text-3xl md:text-4xl font-bold">Projects</h2>
+        <h2>Education</h2>
+        <p>
+          <CircleAlert className="size-4 inline-block" /> We&apos;re keeping it
+          simple! •{" "}
+          <Link
+            href="https://www.aqf.edu.au/framework/aqf-qualifications"
+            className="text-blue-500 hover:text-blue-400"
+            target="_blank"
+          >
+            Australian Qualifications Framework{" "}
+            <ExternalLink className="inline-block size-4" />
+          </Link>
+        </p>
+        <div>
+          <h3>Bachelor of Computer Science</h3>
+          <p className="opacity-75">
+            Minor in Cloud Computing • Graduating 2026
+          </p>
+        </div>
+        <div>
+          <h3>Certificate in Front-End Development</h3>
+          <Link
+            href="https://www.coursera.org/account/accomplishments/professional-cert/K329R2NDRAS9"
+            className="text-blue-500 hover:text-blue-400"
+            target="_blank"
+          >
+            Digital Certificate <ExternalLink className="inline-block size-4" />
+          </Link>
+        </div>
+        <div>
+          <h3>IT Specialist - Python</h3>
+          <Link
+            href="https://www.credly.com/badges/4c90f5d6-40e9-4263-a534-d141cba93c69/linked_in?t=sdxccf"
+            className="text-blue-500 hover:text-blue-400"
+            target="_blank"
+          >
+            Digital Certificate <ExternalLink className="inline-block size-4" />
+          </Link>
+        </div>
+      </section>
+      <hr />
+      <section className="space-y-4">
+        <h2>Projects</h2>
         <div className="space-y-2">
-          <h3 className="text-2xl md:text-3xl font-semibold">WorldMC <Globe className="inline-block text-blue-500"/></h3>
-          <p className="opacity-75">A massive multiplayer online experience centered around a map of the Earth. This website, as well as the backend API which drives it, showcases in-game markets, players, towns, and nations.</p>
+          <h3>
+            RoGold <Blocks className="inline-block text-amber-500" />
+          </h3>
+          <p className="opacity-75">
+            Official website for the RoGold extension, designed for Roblox
+            enthusiasts and developers. Built with Next.js, the site features a
+            sleek and intuitive interface, providing easy access to extension
+            features, support, and updates. Aimed to enhance user experience
+            with seamless navigation and responsiveness across devices.
+          </p>
+          <CollapsibleVideo title="RoGold Demo" />
+          <p>
+            <Link
+              href="https://rogold.live"
+              className="text-blue-500 hover:text-blue-400"
+              target="_blank"
+            >
+              Visit Website <ExternalLink className="inline-block size-4" />
+            </Link>
+          </p>
+        </div>
+        <div className="space-y-2">
+          <h3>
+            WorldMC <Globe className="inline-block text-blue-500" />
+          </h3>
+          <p className="opacity-75">
+            A massive multiplayer online experience centered around a map of the
+            Earth. This website, as well as the backend API which drives it,
+            showcases in-game markets, players, towns, and nations.
+          </p>
           <CollapsibleVideo src="/worldmc.webm" title="WorldMC Demo" />
           <p>
             <Link
-              href="https://worldmc.net" // Replace with actual link
+              href="https://worldmc.net"
               className="text-blue-500 hover:text-blue-400"
               target="_blank"
             >
@@ -94,16 +177,22 @@ export default function Home() {
           </p>
         </div>
         <div className="space-y-2">
-          <h3 className="text-2xl md:text-3xl font-semibold">
+          <h3>
             Shockwaves Radio <Radio className="inline-block text-indigo-500" />
           </h3>
           <p className="opacity-75">
-          A radio website featuring live music streaming, staff management, and partnerships with games like Farming Simulator and Euro Truck Simulator. The platform allows staff to manage playlists, schedules, and roles through a dedicated dashboard.
+            A radio website featuring live music streaming, staff management,
+            and partnerships with games like Farming Simulator and Euro Truck
+            Simulator. The platform allows staff to manage playlists, schedules,
+            and roles through a dedicated dashboard.
           </p>
-          <CollapsibleVideo src="/shockwaves.webm" title="Shockwaves Radio Demo" />
+          <CollapsibleVideo
+            src="/shockwaves.webm"
+            title="Shockwaves Radio Demo"
+          />
           <p>
             <Link
-              href="https://shockwavesradio.com" // Replace with actual link
+              href="https://shockwavesradio.com"
               className="text-blue-500 hover:text-blue-400"
               target="_blank"
             >
@@ -112,12 +201,19 @@ export default function Home() {
           </p>
         </div>
         <div className="space-y-2">
-          <h3 className="text-2xl md:text-3xl font-semibold">RoLinker <Wrench className="inline-block text-red-500"/></h3>
-          <p className="opacity-75">Link Roblox and Discord accounts together by connecting both of them to RoLinker. Access a discord bot which enables commands for Roblox groups, and an optional Roblox bot account to facilitate group management from Discord.</p>
+          <h3>
+            RoLinker <Wrench className="inline-block text-red-500" />
+          </h3>
+          <p className="opacity-75">
+            Link Roblox and Discord accounts together by connecting both of them
+            to RoLinker. Access a discord bot which enables commands for Roblox
+            groups, and an optional Roblox bot account to facilitate group
+            management from Discord.
+          </p>
           <CollapsibleVideo src="/rolinker.webm" title="RoLinker Demo" />
           <p>
             <Link
-              href="https://rolinker.net" // Replace with actual link
+              href="https://rolinker.net"
               className="text-blue-500 hover:text-blue-400"
               target="_blank"
             >
@@ -126,20 +222,37 @@ export default function Home() {
           </p>
         </div>
         <div className="space-y-2">
-          <h3 className="text-2xl md:text-3xl font-semibold">CarePulse <Hospital className="inline-block text-purple-500"/></h3>
-          <p className="opacity-75">A patient management system with a patient-centric design. CarePulse features an admin dashboard for appointment management, a safe sign-on process for new patients, OTP, and SMS appointment verification/confirmation.</p>
+          <h3>
+            CarePulse <Hospital className="inline-block text-purple-500" />
+          </h3>
+          <p className="opacity-75">
+            A patient management system with a patient-centric design. CarePulse
+            features an admin dashboard for appointment management, a safe
+            sign-on process for new patients, OTP, and SMS appointment
+            verification/confirmation.
+          </p>
           <CollapsibleVideo src="/carepulse.webm" title="CarePulse Demo" />
         </div>
         <div className="space-y-2">
-          <h3 className="text-2xl md:text-3xl font-semibold">Arstotzka <Flag className="inline-block text-green-700"/></h3>
-          <p className="opacity-75">Showcasing various custom features such as user interface, marketplace, weapons, economy, NPCs, character customization, and crime. The UI design captures the 1980s aesthetic, following the theme of Arstotzka.</p>
+          <h3>
+            Arstotzka <Flag className="inline-block text-green-700" />
+          </h3>
+          <p className="opacity-75">
+            Showcasing various custom features such as user interface,
+            marketplace, weapons, economy, NPCs, character customization, and
+            crime. The UI design captures the 1980s aesthetic, following the
+            theme of Arstotzka.
+          </p>
           <CollapsibleVideo src="/arstotzka.webm" title="Arstotzka Demo" />
         </div>
       </section>
       <section className="text-center">
         <h3 className="text-xl font-black">That&apos;s all!</h3>
         <p className="opacity-75">Thanks for reading.</p>
-        <Link href="mailto:me@zekepari.dev" className="hover:text-blue-600 transition">
+        <Link
+          href="mailto:me@zekepari.dev"
+          className="hover:text-blue-600 transition"
+        >
           <Mail className="size-4 inline-block mr-1" />
           me@zekepari.dev
         </Link>
--- components/CollapsibleVideo.tsx
+++ components/CollapsibleVideo.tsx
@@ -1,42 +1,62 @@
 "use client";
 
+import clsx from "clsx";
 import { ChevronDown, ChevronUp } from "lucide-react";
-import { useEffect, useRef, useState } from "react";
+import { useState, useRef, useEffect } from "react";
 
-const CollapsibleVideo = ({ src, title }: { src: string; title: string; }) => {
-    const [isOpen, setIsOpen] = useState(false);
-    const [height, setHeight] = useState(0);
-    const contentRef = useRef<HTMLDivElement>(null);
-  
-    useEffect(() => {
-      if (isOpen) {
-        setHeight(contentRef.current?.scrollHeight || 0);
-      } else {
-        setHeight(0);
-      }
-    }, [isOpen]);
-  
-    return (
-      <div className="border rounded-md mb-4 overflow-hidden">
-        <button
-          className="w-full flex justify-between items-center p-4 font-semibold"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {title}
-          {isOpen ? <ChevronUp className="size-5" /> : <ChevronDown className="size-5" />}
-        </button>
-        <div 
-          ref={contentRef}
-          style={{ height: `${height}px` }}
-          className="transition-all duration-300 ease-in-out"
-        >
+interface CollapsibleVideoProps {
+  src?: string | null;
+  title: string;
+}
+
+export default function CollapsibleVideo({
+  src,
+  title,
+}: CollapsibleVideoProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef<HTMLDivElement>(null);
+  const [contentHeight, setContentHeight] = useState(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(contentRef.current.scrollHeight);
+    }
+  }, [isOpen]);
+
+  return (
+    <div className="border rounded-md mb-4">
+      <button
+        className="w-full flex justify-between items-center p-4 font-semibold"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {title}
+        {isOpen ? (
+          <ChevronUp className="size-5" />
+        ) : (
+          <ChevronDown className="size-5" />
+        )}
+      </button>
+
+      <div
+        ref={contentRef}
+        style={{
+          maxHeight: isOpen ? `${contentHeight}px` : "0px",
+        }}
+        className={clsx("overflow-hidden transition-[max-height] duration-300")}
+      >
+        {src ? (
           <video className="w-full" controls>
             <source src={src} type="video/webm" />
             Your browser does not support the video tag.
           </video>
-        </div>
+        ) : (
+          <div className="p-4">
+            <p className="text-gray-500">
+              The video is not available. Please visit the website instead.
+            </p>
+          </div>
+        )}
       </div>
-    );
-  };
-
-export default CollapsibleVideo
+    </div>
+  );
+}
